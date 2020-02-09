cab48adc771327e801821435f9d62f488e241d1f
Kemi Thomas <kemothom71@gmail.com>
2020-02-09 01:33:19 -0500

    adding tables to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,52 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(reservations => reservations.json())
       .then(data => renderReservations(data));
   }
+
   function renderReservations(data) {
-    for (const q of data) {
-      const resList = document.querySelector("#reservation-detail");
-      const resUL = document.createElement("li");
-      resList.append(resUL);
+    for (const r of data) {
+      //Tables
+      const table = document.querySelectorAll("#table-reservation");
+      const tableList = document.querySelector(".dot");
+      console.log("table", tableList);
+      const addReservation1 = document.createElement("button");
+      // const addReservation2 = document.querySelector(".dot #2");
+      // const addReservation3 = document.querySelector(".dot #3");
+      // const addReservation4 = document.querySelector(".dot #4");
+      // const addReservation5 = document.querySelector(".dot #5");
+      // const addReservation6 = document.querySelector(".dot #6");
+      // const addReservation7 = document.querySelector(".dot #7");
+      // const addReservation8 = document.querySelector(".dot #8");
+      // const addReservation9 = document.querySelector(".dot #9");
+      // const addReservation10 = document.querySelector(".dot #10");
+      //Reservations
+      const resUL = document.querySelector("#reservation-detail");
+      const resList = document.createElement("li");
+      console.log("li", resList);
+      const blockQuote = document.createElement("blockquote");
+      console.log("res", blockQuote);
+      const p = document.createElement("p");
+      const footer = document.createElement("footer");
+      const br = document.createElement("br");
+      const hr = document.createElement("hr");
+
+      resList.className = "res-card";
+      // tableList.className = "dot";sdf
+      blockQuote.className = "blockQuote";
+      p.className = "mb-0";
+      footer.className = "blockquote-footer";
+      // resList.dataset.id = r.id;
+
+      // p.innerHTML = r.name;
+      // footer.innerHTML = r.slot;
+
+      table.append(addReservation1);
+      tableList.append(table);
+      // table.append(tableList);
+      // resUL.append(resList);
+
+      addReservation1.innerHTML = "Reserve Table";
+      addReservation1.className = "something";
+      addReservation1.addEventListener("click", () => addReservation());
     }
   }
   fetchData();
--- seeders/20200207010327-demo-reservation.js
+++ seeders/20200207010327-demo-reservation.js
@@ -1,24 +1,31 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Reservations', [
+    return queryInterface.bulkInsert("Reservations", [
       {
-        name: 'John Cena',
-        slot: new Date('21 Feb 2020 17:00:00 GMT-0500'),
+        name: "John Cena",
+        slot: new Date("21 Feb 2020 17:00:00 GMT-0500"),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        name: 'Not John Cena',
-        slot: new Date('21 Feb 2020 18:30:00 GMT-0500'),
+        name: "Not John Cena",
+        slot: new Date("21 Feb 2020 18:30:00 GMT-0500"),
         createdAt: new Date(),
         updatedAt: new Date()
-      }
+      },
+      queryInterface.bulkDelete("Table", [
+        {
+          id: 1,
+          name: 1
+        },
+        { id: 2, name: 2 }
+      ])
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Reservations', null, {});
+    return queryInterface.bulkDelete("Reservations", null, {});
   }
 };
